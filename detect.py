0ee7ff086f4a58233653d1987228a0df26f9e0af
Daniel Bobritski <danielbob32@gmail.com>
2024-03-28 13:55:28 +0200

    Re-ordeing files and changes to jupyter notebook

--- detect.py
+++ detect.py
@@ -1,5 +1,4 @@
 #%%
-import torch
 from ultralytics import YOLO
 
 
@@ -8,5 +7,3 @@ model = YOLO('yolov8m.pt')
 
 results = model(source='Assets/1.jpg', show=False, conf=0.05, save=True, classes=2, save_txt=True, save_conf=True, line_width=1)
 
-for r in results:
-    print(r.masks)
---
+++ masks.py
@@ -0,0 +1,25 @@
+#%%
+import torch
+from ultralytics import YOLO
+
+model = YOLO('yolov8n-seg.pt')
+
+results = model(source='Assets/Baseline Images/1.jpg', show=False, conf=0.05, save=True, classes=2, save_txt=True, save_conf=True, line_width=1)
+
+# Assuming the results object has an attribute 'masks'
+if hasattr(results[0], 'masks') and results[0].masks is not None:
+    # Prepare the text content
+    mask_texts = []
+    for i, mask in enumerate(results[0].masks):
+        mask_text = f"Car #{i+1} Mask Coordinates:\n{mask}\n"
+        mask_texts.append(mask_text)
+    
+    # Write the mask data to a text file
+    with open('car_masks.txt', 'w') as f:
+        for mask_text in mask_texts:
+            f.write(mask_text)
+
+    print("Masks data saved to car_masks.txt")
+else:
+    print("No masks data found in the results.")
+# %%
