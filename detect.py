428f36fa047892f796d4b762bf911e60d8b3bea2
Daniel Bobritski <danielbob32@gmail.com>
2024-03-28 15:39:08 +0200

    Added juypter updates

--- detect.py
+++ detect.py
@@ -2,8 +2,8 @@
 from ultralytics import YOLO
 
 
-model = YOLO('yolov8m.pt')
+model = YOLO('yolov8x.pt')
 
 
-results = model(source='Assets/1.jpg', show=False, conf=0.05, save=True, classes=2, save_txt=True, save_conf=True, line_width=1)
+results = model(source='Assets/Baseline Images/1.jpg', show=False, conf=0.05, save=True, classes=2, save_txt=True, save_conf=True, line_width=1)
 
--- masks.py
+++ masks.py
@@ -2,7 +2,7 @@
 import torch
 from ultralytics import YOLO
 
-model = YOLO('yolov8n-seg.pt')
+model = YOLO('yolov8x-seg.pt')
 
 results = model(source='Assets/Baseline Images/1.jpg', show=False, conf=0.05, save=True, classes=2, save_txt=True, save_conf=True, line_width=1)
 
